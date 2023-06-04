1e767698dd09b71fe816c664984a41d0e3a2e80d
marcelo <marcelegald2@outlook.com>
2023-06-04 19:22:56 -0300

    [M2S09] Ex 9 - Controller Filmes

--- Filmes/Program.cs
+++ Filmes/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1,0);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
